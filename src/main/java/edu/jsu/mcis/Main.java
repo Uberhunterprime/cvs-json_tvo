d3bbcfb4184e70abbb9826546932350b53e56c5c
Uberhunterprime <tv.yo993@yahoo.com>
2017-02-03 12:08:40 -0600

    update

---
+++ src/main/java/edu/jsu/mcis/Main.java
@@ -0,0 +1,36 @@
+package edu.jsu.mcis;
+
+// C:\Users\tvyo9\Desktop\Git Repository\cvs-json_tvo\src\main\java\edu\jsu\mcis
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        ClassLoader loader = ClassLoader.getSystemClassLoader();
+        StringBuffer csvContents = new StringBuffer();
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(loader.getResourceAsStream("grades.csv")))) {
+            String line;
+            while((line = reader.readLine()) != null) {
+                csvContents.append(line + '\n');
+            }
+        }
+        catch(IOException e) { e.printStackTrace(); }
+        String testCsv = csvContents.toString();
+        
+        StringBuffer jsonContents = new StringBuffer();
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(loader.getResourceAsStream("grades.json")))) {
+            String line;
+            while((line = reader.readLine()) != null) {
+                jsonContents.append(line + '\n');
+            }
+        }
+        catch(IOException e) { e.printStackTrace(); }
+        String testJson = jsonContents.toString();
+
+        String json = Converter.csvToJson(testCsv);
+        System.out.println(json);
+        System.out.println("\n----------------\n");
+        String csv = Converter.jsonToCsv(testJson);
+        System.out.println(csv);
+    }
+}
